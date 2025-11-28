980b8514a88515db11682901a0e647cd9a5fce06
rekha kumawat <rekhasumanku@gmail.com>
2025-11-28 12:00:18 +0530

    program123.c with comments and formatting
    Added documentation and improved formatting in program123.c.

--- C_Program/Classwork/Day_7/program123.c
+++ C_Program/Classwork/Day_7/program123.c
@@ -1,5 +1,22 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//required  package
+//
+///////////////////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
 #include<stdlib.h>
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Display
+//  Description :   it is used to Display elements of aaray
+//  Input :         Integer
+//  output :        void
+//  Author :        Rekha Shankarlal Kumawat
+//  Date :          13/11/2025
+//
+///////////////////////////////////////////////////////////////////////////////
 
 void Display(int Arr[] , int iSize)
 {
@@ -10,6 +27,11 @@ void Display(int Arr[] , int iSize)
         printf("%d ", Arr[iCnt]);
     }
 }
+///////////////////////////////////////////////////////////////////////////////
+//
+//Entry point function
+//
+///////////////////////////////////////////////////////////////////////////////
 
 int main()
 {
@@ -20,7 +42,7 @@ int main()
     printf("Enter number of elements :");
     scanf("%d",&iLength);
 
-    ptr = (int*)malloc(iLength * sizeof(int));
+    ptr = (int*)malloc(iLength * sizeof(int));        //dynamic memory allocation
 
     printf("Enter the elements : \t");
 
@@ -31,7 +53,14 @@ int main()
 
     Display(ptr , iLength);
 
-    free(ptr);
+    free(ptr);                                    // memory deallocation
 
     return 0 ;
+
 }
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Input:1 2 3 4                  Output:1 2 3 4 
+//  Input:1 1 2 3                  Output:1 1 2 3 
+//
+////////////////////////////////////////////////////////////////////
