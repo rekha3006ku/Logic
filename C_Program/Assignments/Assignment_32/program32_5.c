74c99b738a836215a1a175ebee883d608499c6e7
rekha kumawat <rekhasumanku@gmail.com>
2025-11-29 23:46:16 +0530

    Add files via upload

---
+++ C_Program/Assignments/Assignment_32/program32_5.c
@@ -0,0 +1,83 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//required header file
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>                                              // for input output
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Display
+//  Description :   Display the pattern of character
+//  Input :         Integer
+//  output :        Void
+//  Author :        Rekha Shankarlal Kumawat
+//  Date :          29/11/2025
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void Display(int iRow , int iCol)
+{
+    int iCnt1 = 0 ;
+    int iCnt2 = 0 ;
+    if(iRow != iCol)
+        {
+            printf("Invalid input");
+            printf("Row number and column number should be same");
+            return;
+        }
+    for(iCnt1 = 1 ; iCnt1 <= iRow ; iCnt1++)
+    {
+        
+        for(iCnt2 = 1; iCnt2 <= iCol ; iCnt2++)
+        {
+            if(iCnt1 == iCnt2 || iCnt1 == 1 || iCnt2== 1 || iCnt1== iRow ||iCnt2== iCol)
+            {
+                printf("%d\t", iCnt2);
+            }
+            else
+            {
+                printf("\t");
+            }
+        }
+        printf("\n");
+    }
+}//end of the Display function
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//Entery point function
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0 ;
+    int iValue2 = 0 ;
+
+    printf("Enter the number of rows:");
+    scanf("%d", &iValue1);
+
+    printf("Enter the number of columns:");
+    scanf("%d", &iValue2);
+
+    Display(iValue1 , iValue2);
+
+    return 0 ;
+}//end of main function
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Input1:4    Input2:4   Output:1       2       3       4
+//                                1       2               4
+//                                1               3       4
+//                                1       2       3       4            
+//
+//  Input1:5    Input2:5   Output:1       2       3       4       5
+//                                1       2                       5
+//                                1               3               5
+//                                1                       4       5
+//                                1       2       3       4       5          
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
